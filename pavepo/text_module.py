b2721b4944c6e78f6d1f65f95c7149f4202c675c
alxvmr <alexeeva.maria16@gmail.com>
2024-04-23 15:34:10 +0400

    Implemented a function to extract audio track and save to the cloud

--- pavepo/text_module.py
+++ pavepo/text_module.py
@@ -1,9 +1,10 @@
 # извлечение текста из видео
 from . import s3
 import moviepy.editor as mpe
+import os
 
-def get_video(video_name, bucket=None):
-    # Получение видео из хранилища по API
+def get_audio_from_video(video_name, bucket=None):
+    # Получение зука из видео
     video = None
     # Получение видео из хранилища по API
     if not bucket:
@@ -12,15 +13,20 @@ def get_video(video_name, bucket=None):
         video = s3.get_object(video_name, bucket=bucket)["Body"]
     if video:
         presignedurl = s3.create_presigned_url(video_name)
-        vidclip = mpe.VideoFileClip(presignedurl)
-        return vidclip
+        audioclip = mpe.AudioFileClip(presignedurl)
+        # сохранение аудиодорожки в хранилище
+        path_audio = f"./temp/{video_name}_audio.mp3"
+        audioclip.write_audiofile(path_audio)
+        s3.upload_object(path_audio)
+        # удаление из локального хранилища
+        os.remove(path_audio)
+        return audioclip
     return None
 
 
 def get_text_from_video(video_name, bucket=None):
-    # Получение текста из видео
-    video = get_video(video_name, bucket)
-    video.preview(fps=20)
+    # Получение звука из видео
+    audioclip = get_audio_from_video(video_name, bucket)
         
 
 if __name__ == "__main__":
